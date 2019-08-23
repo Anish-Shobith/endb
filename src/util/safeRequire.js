fbfacebeba50c4f67dd72f6342ac80c800907839
chroventer <chroventer@gmail.com>
2019-08-23 04:19:45 -0700

    refactor: remove unnecessary code

--- src/util/safeRequire.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(module) {
-    try {
-        return require(module);
-    } catch (err) {
-        throw new Error(`Run "npm i ${module}" to install the package`);
-    }
-};
