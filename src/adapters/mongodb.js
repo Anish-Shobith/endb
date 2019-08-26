964cc91cf0655b26771f44c956da24d647b09131
chroventer <chroventer@gmail.com>
2019-08-27 00:13:26 +0400

    fix: remove key checks

--- src/adapters/mongodb.js
+++ src/adapters/mongodb.js
@@ -6,62 +6,59 @@ const { safeRequire } = require('../util');
 const mongojs = safeRequire('mongojs');
 
 class EndbMongo extends EventEmitter {
-    constructor(url, options = {}) {
-        super();
-        url = url || {};
-        if (typeof url === 'string') {
-            url = { url };
-        }
-        if (url.uri) {
-            url = Object.assign({ url: url.uri }, url);
-        }
-        options = Object.assign({
-            url: 'mongodb://127.0.0.1:27017',
-            collection: 'endb'
-        }, url, options);
-        const mongo = mongojs(options.uri);
-        const collection = mongo.collection(options.collection);
-        collection.createIndex({ key: 1 }, {
-            unique: true,
-            background: true
-        });
-        this.db = ['update', 'find', 'findOne', 'remove'].reduce((obj, method) => {
-            obj[method] = promisify(collection[method].bind(collection));
-            return obj;
-        }, {});
-        mongo.on('error', err => this.emit('error', err));
-    }
+	constructor(url, options = {}) {
+		super();
+		url = url || {};
+		if (typeof url === 'string') {
+			url = { url };
+		}
+		if (url.uri) {
+			url = Object.assign({ url: url.uri }, url);
+		}
+		options = Object.assign({
+			url: 'mongodb://127.0.0.1:27017',
+			collection: 'endb'
+		}, url, options);
+		const mongo = mongojs(options.uri);
+		const collection = mongo.collection(options.collection);
+		collection.createIndex({ key: 1 }, {
+			unique: true,
+			background: true
+		});
+		this.db = ['update', 'find', 'findOne', 'remove'].reduce((obj, method) => {
+			obj[method] = promisify(collection[method].bind(collection));
+			return obj;
+		}, {});
+		mongo.on('error', err => this.emit('error', err));
+	}
 
-    all() {
-        return this.db.find()
-            .then(data => {
-                return data.map(doc => doc === null ? undefined : doc.value);
-            });
-    }
+	all() {
+		return this.db.find()
+			.then(data => {
+				return data.map(doc => doc === null ? undefined : doc.value);
+			});
+	}
 
-    clear() {
-        return this.db.remove({ key: new RegExp(`^${this.namespace}:`) })
-            .then(() => undefined);
-    }
+	clear() {
+		return this.db.remove({ key: new RegExp(`^${this.namespace}:`) })
+			.then(() => undefined);
+	}
 
-    delete(key) {
-        if (typeof key !== 'string') {
-            return Promise.resolve(false);
-        }
-        return this.db.remove({ key })
-            .then(data => data.n > 0);
-    }
+	delete(key) {
+		return this.db.remove({ key })
+			.then(data => data.n > 0);
+	}
 
-    get(key) {
-        return this.db.findOne({ key })
-            .then(data => {
-                return data === null ? undefined : data.value;
-            });
-    }
+	get(key) {
+		return this.db.findOne({ key })
+			.then(data => {
+				return data === null ? undefined : data.value;
+			});
+	}
 
-    set(key, value) {
-        return this.db.update({ key }, { key, value }, { upsert: true });
-    }
+	set(key, value) {
+		return this.db.update({ key }, { key, value }, { upsert: true });
+	}
 }
 
 module.exports = EndbMongo;
--- src/adapters/redis.js
+++ src/adapters/redis.js
@@ -6,64 +6,61 @@ const { safeRequire } = require('../util');
 const { createClient } = safeRequire('redis');
 
 class EndbRedis extends EventEmitter {
-    constructor(uri, options = {}) {
-        super();
-        options = Object.assign({}, typeof uri === 'string' ? { uri } : uri, options);
-        if (options.uri && typeof options.url === 'undefined') {
-            options.url = options.uri;
-        }
-        const client = createClient(options);
-        this.db = ['get', 'keys', 'set', 'sadd', 'del', 'srem', 'smembers'].reduce((obj, method) => {
-            obj[method] = promisify(client[method].bind(client));
-            return obj;
-        }, {});
-        client.on('error', (err) => this.emit('error', err));
-    }
+	constructor(uri, options = {}) {
+		super();
+		options = Object.assign({}, typeof uri === 'string' ? { uri } : uri, options);
+		if (options.uri && typeof options.url === 'undefined') {
+			options.url = options.uri;
+		}
+		const client = createClient(options);
+		this.db = ['get', 'keys', 'set', 'sadd', 'del', 'srem', 'smembers'].reduce((obj, method) => {
+			obj[method] = promisify(client[method].bind(client));
+			return obj;
+		}, {});
+		client.on('error', (err) => this.emit('error', err));
+	}
 
-    all() {
-        return this.db.keys('*')
-            .then(data => {
-                for (let i = 0; i < data.length; i++) {
-                    return data[i] === null ? null : data[i];
-                }
-            });
-    }
+	all() {
+		return this.db.keys('*')
+			.then(data => {
+				for (let i = 0; i < data.length; i++) {
+					return data[i] === null ? null : data[i];
+				}
+			});
+	}
 
-    clear() {
-        return this.db.smembers(this._prefixNamespace())
-            .then(data => this.db.del.apply(null, data.concat(this._prefixNamespace())))
-            .then(() => undefined);
-    }
+	clear() {
+		return this.db.smembers(this._prefixNamespace())
+			.then(data => this.db.del.apply(null, data.concat(this._prefixNamespace())))
+			.then(() => undefined);
+	}
 
-    delete(key) {
-        return this.db.del(key)
-            .then(data => {
-                return this.db.srem(this._prefixNamespace(), key)
-                    .then(() => data > 0);
-            });
-    }
+	delete(key) {
+		return this.db.del(key)
+			.then(data => {
+				return this.db.srem(this._prefixNamespace(), key)
+					.then(() => data > 0);
+			});
+	}
 
-    get(key) {
-        return this.db.get(key)
-            .then(data => {
-                return data === null ? null : data;
-            });
-    }
+	get(key) {
+		return this.db.get(key)
+			.then(data => {
+				return data === null ? null : data;
+			});
+	}
 
-    set(key, value) {
-        if (typeof value === 'undefined') {
-            return Promise.resolve(undefined);
-        }
-        return Promise.resolve()
-            .then(() => {
-                return this.db.set(key, value);
-            })
-            .then(() => this.db.sadd(this._prefixNamespace(), key));
-    }
+	set(key, value) {
+		return Promise.resolve()
+			.then(() => {
+				return this.db.set(key, value);
+			})
+			.then(() => this.db.sadd(this._prefixNamespace(), key));
+	}
 
-    _prefixNamespace() {
-        return this.namespace ? `namespace:${this.namespace}` : undefined;
-    }
+	_prefixNamespace() {
+		return this.namespace ? `namespace:${this.namespace}` : undefined;
+	}
 }
 
 module.exports = EndbRedis;
